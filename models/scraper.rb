b62f65cf1da6ba45130ae46c62f840aa6d725fd8
SVR <samuel@rourke.tech>
2020-08-06 16:39:10 -0400

    began work on api request functionality

---
+++ models/scraper.rb
@@ -0,0 +1,11 @@
+class Scraper
+    def initialize()
+        grtr = '>'
+        @base_url = "https://api.bestbuy.com/v1/products(customerReviewAverage#{grtr}=4&customerReviewCount>100&onSale=true)?show=name,url,customerReviewAverage,priceUpdateDate,salePrice,dollarSavings,sku&format=json&pageSize=50&page=50&apiKey=AngmDMbjxfx9GEcQ2kDDTwtk"
+        
+    end
+
+    def getResults
+        RestClient.get(@base_url)
+    end
+end
