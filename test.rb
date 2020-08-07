6c4f7652785cc5d5908fef38e0962b828722f43b
SVR <samuel@rourke.tech>
2020-08-07 17:52:57 -0400

    more work on the request mechanics

--- test.rb
+++ test.rb
@@ -3,22 +3,24 @@ require 'json'
 require 'pry'
 
 base_url = "https://api.bestbuy.com/v1/products(customerReviewAverage>4&customerReviewCount>100&onSale=true)?show=name,url,customerReviewAverage,priceUpdateDate,salePrice,dollarSavings,sku&format=json&pageSize=50&page=50&apiKey=AngmDMbjxfx9GEcQ2kDDTwtk"
-base = "https://api.bestbuy.com/v1/products(customerReviewAverage>4&customerReviewCount>100&onSale=true)?show=name,url,customerReviewAverage,priceUpdateDate,salePrice,dollarSavings,sku"
+base = "https://api.bestbuy.com/v1/products(customerReviewAverage>4&customerReviewCount>100&onSale=true)"
 
 # uri = URI(base_url)
 # resp = HTTParty.get(uri)
+
+
 
 # req = Typhoeus::Request.new(base_url).run
 req = Typhoeus::Request.new(
     base,
     method: :get,
     params: {
+        show: "name,url,customerReviewAverage,priceUpdateDate,salePrice,dollarSavings,sku",
         format: 'json',
         pageSize: 50,
         page: 5,
         apiKey: 'AngmDMbjxfx9GEcQ2kDDTwtk'
-    }
-).run
+    }).run
 
 body = JSON.parse(req.response_body)
 
