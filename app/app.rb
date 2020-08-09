95b5c0e7a09f05a04ddd72738f8124774890182c
SVRourke <samuel@rourke.tech>
2020-08-09 15:48:13 -0400

    AWESOME

--- app/app.rb
+++ app/app.rb
@@ -2,24 +2,41 @@ require_relative './../config/environment.rb'
 
 class App < Sinatra::Base
     enable :sessions
+    set :session_secret, 'secret sauce'
 
-    get '/page' do
-        redirect to('/page/1')
+    get '/' do
+        redirect to('/pages/1')
+    end    
+        
+    get '/pages' do
+        redirect to('/pages/1')
     end
 
-    post '/page' do
-        # redirect to('/test')
-        session[:filters] = "SAVED FILTER"
-        redirect to('/page/1')
+    post '/filter' do 
+        
+        session[:filters] = params
+        redirect to('/pages/1')
     end
 
-     get '/page/:num' do
-        ix = DataInterface.new('AngmDMbjxfx9GEcQ2kDDTwtk')
-        @data = ix.getPage(page = params[:num])
-        @login = session[:filters]
-        erb :layout
+    get '/pages/:num' do
+        if session[:filters]
+            ix = DataInterface.new('AngmDMbjxfx9GEcQ2kDDTwtk')
+            @data = ix.getPagenew(session[:filters], page = params[:num])
+            erb :layout
+        else
+            redirect to('/default-filters')
+        end
     end
 
-
+    get '/default-filters' do
+        session[:filters] = {
+            "pageSize"=>"25",
+            "salePrice"=>">=10",
+            "customerReviewAverage"=>">3",
+            "customerReviewCount"=>">10",
+            "inStorePickup"=>""
+        }
+        redirect to '/pages/1'
+    end
 
 end
--- models/scraper.rb
+++ models/scraper.rb
@@ -5,38 +5,30 @@ class DataInterface
         @base = "https://api.bestbuy.com/v1/products(customerReviewAverage>4&customerReviewCount>100&onSale=true)"
     end
     
-    def getPage(page = 1)
-        req = Typhoeus::Request.new(
-            @base,
-            method: :get,
-            params: {
-                show: @display,
-                format: 'json',
-                pageSize: 100,
-                page: page,
-                apiKey: @key
-        }).run
+    # def getPage(page = 1)
+    #     req = Typhoeus::Request.new(
+    #         @base,
+    #         method: :get,
+    #         params: {
+    #             show: @display,
+    #             format: 'json',
+    #             pageSize: 100,
+    #             page: page,
+    #             apiKey: @key
+    #     }).run
 
-        body = JSON.parse(req.response_body)
+    #     body = JSON.parse(req.response_body)
 
-        results = {
-            :page => body['currentPage'],
-            :pages => body['totalPages'],
-            :products => body['products']
-        }
-    end
+    #     results = {
+    #         :page => body['currentPage'],
+    #         :pages => body['totalPages'],
+    #         :products => body['products']
+    #     }
+    # end
     
-    def getPage(page = 1)
-        req = Typhoeus::Request.new(
-            @base,
-            method: :get,
-            params: {
-                show: @display,
-                format: 'json',
-                pageSize: 100,
-                page: page,
-                apiKey: @key
-        }).run
+    def getPagenew(filters, page)
+        req = buildRequest(filters, page )
+        # req.run
 
         body = JSON.parse(req.response_body)
 
@@ -47,29 +39,34 @@ class DataInterface
         }
     end
 
-    def buildBase()
-        "customerReviewAverage>4&customerReviewCount>100&onSale=true"
-        b_url = "https://api.bestbuy.com/v1/products(#{filters})"
+    def buildBase(filters)
+        base = []
+        filters.each do |key, value|
+            if value != "" && key != "pageSize"
+                base << "#{key.to_s}#{value}"
+            end                
+        end
+        b_url = "https://api.bestbuy.com/v1/products(#{base.join("&")}&onSale=true)"
     end
 
 
-    def buildRequest(base_url, pageSize=20, page )
+    def buildRequest(filters, page )
         attribs = "name,url,customerReviewAverage,priceUpdateDate,salePrice,dollarSavings,sku,image"
+        base_url = buildBase(filters)
+        puts base_url
         req = Typhoeus::Request.new(
             base_url,
             method: :get,
             params: {
                 show: attribs,
                 format: 'json',
-                pageSize: pageSize,
+                pageSize: filters["pageSize"],
                 page: page,
                 apiKey: @key
-        })
+        }).run
     end
 
 
 end
-
-
 
 
