bd7212b0ab590ad21237c4340c0294dd6912d696
SVRourke <samuel@rourke.tech>
2020-08-09 13:42:15 -0400

    modified scraper and layouts switched to bootstrap

--- app/app.rb
+++ app/app.rb
@@ -1,17 +1,25 @@
 require_relative './../config/environment.rb'
 
 class App < Sinatra::Base
+    enable :sessions
 
     get '/page' do
         redirect to('/page/1')
     end
 
+    post '/page' do
+        # redirect to('/test')
+        session[:filters] = "SAVED FILTER"
+        redirect to('/page/1')
+    end
 
      get '/page/:num' do
         ix = DataInterface.new('AngmDMbjxfx9GEcQ2kDDTwtk')
         @data = ix.getPage(page = params[:num])
+        @login = session[:filters]
         erb :layout
     end
+
 
 
 end
--- models/scraper.rb
+++ models/scraper.rb
@@ -1,14 +1,3 @@
-# class Scraper
-#     def initialize()
-#         grtr = '>'
-#         @base_url = "https://api.bestbuy.com/v1/products(customerReviewAverage#{grtr}=4&customerReviewCount>100&onSale=true)?show=name,url,customerReviewAverage,priceUpdateDate,salePrice,dollarSavings,sku&format=json&pageSize=50&page=50&apiKey=AngmDMbjxfx9GEcQ2kDDTwtk"
-        
-#     end
-
-#     def getResults
-#         RestClient.get(@base_url)
-#     end
-# end
 class DataInterface
     def initialize(key = '')
         @key = key
@@ -36,16 +25,51 @@ class DataInterface
             :products => body['products']
         }
     end
+    
+    def getPage(page = 1)
+        req = Typhoeus::Request.new(
+            @base,
+            method: :get,
+            params: {
+                show: @display,
+                format: 'json',
+                pageSize: 100,
+                page: page,
+                apiKey: @key
+        }).run
 
-    def getProducts
-        @products = []
-        base_req = getPage(page = 1)
+        body = JSON.parse(req.response_body)
 
-        (1..base_req[:pages]).each do |p|
-            sleep(0.5)
-            puts self.getPage(page = p)[:products].count
-        end
-
-        @products
+        results = {
+            :page => body['currentPage'],
+            :pages => body['totalPages'],
+            :products => body['products']
+        }
     end
+
+    def buildBase()
+        "customerReviewAverage>4&customerReviewCount>100&onSale=true"
+        b_url = "https://api.bestbuy.com/v1/products(#{filters})"
+    end
+
+
+    def buildRequest(base_url, pageSize=20, page )
+        attribs = "name,url,customerReviewAverage,priceUpdateDate,salePrice,dollarSavings,sku,image"
+        req = Typhoeus::Request.new(
+            base_url,
+            method: :get,
+            params: {
+                show: attribs,
+                format: 'json',
+                pageSize: pageSize,
+                page: page,
+                apiKey: @key
+        })
+    end
+
+
 end
+
+
+
+
